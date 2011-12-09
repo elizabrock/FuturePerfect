c22bce806bd917c8c457cc1ea754bef6550de29d
Eliza Brock <eliza@elizabrocksoftware.com>
2011-12-09 14:45:42 -0600

    Remove old project files

--- app/models/user.rb
+++
@@ -1,8 +0,0 @@
-class User < ActiveRecord::Base
-  has_many :goals
-  has_many :top_level_goals, :class_name => "Goal", :conditions => {:parent_goal_id => nil}
-  
-  devise :authenticatable, :confirmable, :recoverable, :rememberable, :registerable, :trackable, :validatable
-
-  attr_accessible :email, :password, :password_confirmation
-end
--- config/initializers/cookie_verification_secret.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random, 
-# no regular words or you'll be exposed to dictionary attacks.
-ActionController::Base.cookie_verifier_secret = '24f4664fa1b6977bc4f14c7cbe52deb24072644373454399ad37684beee1c42be34e10f4af04ecc184ee0b0ca98af823bc8aa78a15ed45647754344b9420f10f'
